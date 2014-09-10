33ee7b27c2b6138f9e226bb1431fb820c01b6dd9
Ben <bewing91@gmail.com>
2014-09-10 14:37:27 -0400

    Finish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,16 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "The Collective"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "The Collective - About"
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
+    assert_select "title", "The Collective - Contact"
   end
 
 end
